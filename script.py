0e60d6ba01624ca3a816fa3dbdde927f558aa09b
pierredethoisy <pierre.dethoisy@gitguardian.com>
2025-03-26 14:32:55 +0100

    bla bla

--- script.py
+++ script.py
@@ -1,16 +1,15 @@
 import requests
 
-# Define your GitGuardian API key
-API_KEY = 'ksdfhjgkejhgekr235346dfdfshtb3254632323432536DFGgdfgdhfdf164535'
+# Define your GitGuardian API key - 
+API_KEY = "ksdfhjgkejhgekr235346dfdfshtb3254632323432536DFGgdfgdhfdf164535"
 
 # Define the GitGuardian API URL
 BASE_URL = 'https://api.gitguardian.com/v1/secrets'
-
+l
 # Create headers with your API key for authentication
 headers = {
     'Authorization': f'Bearer {API_KEY}'
-}
-
+} ;
 def fetch_secrets():
     try:
         # Make a GET request to retrieve the list of secrets
@@ -20,7 +19,7 @@ def fetch_secrets():
         if response.status_code == 200:
             secrets_data = response.json()
             # Print the list of secrets
-            print(f"Secrets retrieved: {secrets_data}")
+            print(f"Secrets retrieved!: {secrets_data}")
         else:
             print(f"Failed to fetch secrets. Status code: {response.status_code}, Error: {response.text}")
     except requests.exceptions.RequestException as e:
